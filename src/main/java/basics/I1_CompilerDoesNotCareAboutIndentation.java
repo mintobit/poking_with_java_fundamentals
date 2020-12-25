5ff5f731d482058c8a9e387292303e5d3292ff7b
Anton Nizhegorodov <mintobit@gmail.com>
2020-12-25 16:11:54 +0200

    Show that Java compiler does not care about indentation

---
+++ src/main/java/basics/I1_CompilerDoesNotCareAboutIndentation.java
@@ -0,0 +1,3 @@
+package basics;
+
+class I1_CompilerDoesNotCareAboutIndentation {public static void main(String[]args){System.out.println(message());}private static String message(){return"It works!";}}
