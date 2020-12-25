a0698e037f4956fc6ae983814aa270799b8d70a5
Anton Nizhegorodov <mintobit@gmail.com>
2020-12-25 16:13:04 +0200

    Explain valid/invalid identifiers in Java

---
+++ src/main/java/basics/I2_Identifiers.java
@@ -0,0 +1,17 @@
+package basics;
+
+class I2_Identifiers {
+    public static void main(String[] args) {
+        // valid identifier names (dollar-sign is not intended for general use)
+        double avgTemp = 5.35;
+        int count = 1;
+        char a4 = 'a';
+        boolean $test = true;
+        String this_is_ok = "John";
+
+        // invalid identifiers
+//        int 2count = 1;
+//        boolean high-temp = true;
+//        boolean w/o = true;
+    }
+}
