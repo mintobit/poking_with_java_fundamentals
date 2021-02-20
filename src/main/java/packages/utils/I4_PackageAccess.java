9f472fc4f4a4473bb4e9b5782182c28fb6f51d8e
Anton Nizhegorodov <mintobit@gmail.com>
2021-02-20 12:26:21 +0200

    Explain class access modifiers

---
+++ src/main/java/packages/utils/I4_PackageAccess.java
@@ -0,0 +1,14 @@
+package packages.utils;
+
+import packages.utils.integer.Multiply;
+
+public class I4_PackageAccess {
+    public static void main(String[] args) {
+        // We can not access this class because it does not have `public` access modifier
+        // Classes without this access modifier are "package private" (accessible only by members of its package)
+        // packages.utils.integer.DivideBy2.execute(10);
+
+        // class packages.utils.integer.Multiply is accessible because it has `public` access modifier
+        System.out.println(Multiply.execute(10, 2));
+    }
+}
---
+++ src/main/java/packages/utils/integer/DivideBy2.java
@@ -0,0 +1,7 @@
+package packages.utils.integer;
+
+class DivideBy2 {
+    public static int execute(int input) {
+        return input / 2;
+    }
+}
